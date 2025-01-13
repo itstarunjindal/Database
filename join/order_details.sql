e9862dabce50d8cd9f6ef4c62f93605c893e7f23
Tarun Jindal <101278379+itstarunjindal@users.noreply.github.com>
2025-01-13 11:44:15 +0530

    [tarun] ADD: [200~Problem: Find the top-performing department based on total sales.

---
+++ join/order_details.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE
+CREATE TABLE
+CREATE TABLE
+CREATE TABLE
+INSERT 0 2
+ id | name 
+----+------
+  1 | A
+  2 | B
+(2 rows)
+
+INSERT 0 5
+ id |  name  | department_id 
+----+--------+---------------
+  1 | anjali |             1
+  2 | rajesh |             2
+  3 | tarun  |             1
+  4 | varun  |             1
+  5 | aman   |             2
+(5 rows)
+
+INSERT 0 5
+ sale_id | employee_id | amount 
+---------+-------------+--------
+       1 |           1 |   5000
+       2 |           2 |   7000
+       3 |           3 |  12000
+       4 |           4 |   3000
+       5 |           5 |  10000
+(5 rows)
+
+ id | name | total_sales 
+----+------+-------------
+  2 | B    |       17000
+  1 | A    |       20000
+(2 rows)
+
+ id | name | total_sales 
+----+------+-------------
+  1 | A    |       20000
+(1 row)
+
